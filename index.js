0674674b5b15b3f1371b0bb87de24b6efbbd1b1a
Adrian Frith <adrian@adrianfrith.com>
2016-02-13 16:40:01 +0200

    express framework

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+var express = require('express');
+
 var db = require('./db'),
   fetch = require('./fetch');
 
 var interval = 2 * 60 * 1000; // 2 minutes
+
+var port = parseInt(process.env.PORT) || 3000;
+
+var app = express();
+app.use('/', require('./routes'));
 
 db.connect(function (err, database) {
   if (err) {
@@ -9,8 +16,12 @@ db.connect(function (err, database) {
     return;
   }
 
-  fetch.fetchAndSave();
+  //fetch.fetchAndSave();
   setInterval(function() {
     fetch.fetchAndSave();
   }, interval);
+
+  app.listen(port, function () {
+    console.log('Listening on port ' + port);
+  });
 });
