2dd95523ee2e35531a383d81cb9b6c4729d50887
Adrian Frith <adrian@adrianfrith.com>
2016-02-13 16:19:32 +0200

    split fetching code out of index.js

---
+++ fetch.js
@@ -0,0 +1,54 @@
+var request = require('request'),
+  querystring = require('querystring'),
+  async = require('async');
+
+var db = require('./db');
+
+var url_base = 'https://maps.googleapis.com/maps/api/directions/json?'
+var api_key = process.env.API_KEY;
+
+var origin = process.env.ORIGIN || '50 Parliament Street, Cape Town';
+var destination = process.env.DESTINATION || '60 Klipper Road, Rondebosch, Cape Town';
+
+function requestFunction(traffic_model) {
+  return function (cb) {
+    var query = {
+      origin: origin,
+      destination: destination,
+      key: api_key,
+      mode: 'driving',
+      departure_time: 'now',
+      traffic_model: traffic_model
+    };
+
+    var url = url_base + querystring.stringify(query);
+
+    request(url, function (err, resp, body) {
+      if (err) return cb(err);
+      if (resp.statusCode != 200) return cb('Request failed, status code ' + resp.statusCode);
+
+      var data = JSON.parse(body);
+      if (data.status != 'OK') return cb('Request unsuccessful, status ' + data.status);
+
+      cb(null, data.routes[0].legs[0].duration_in_traffic.value);
+    });
+  }
+}
+
+exports.fetchAndSave = function () {
+  var time = new Date();
+  async.parallel({
+    best_guess: requestFunction('best_guess'),
+    pessimistic: requestFunction('pessimistic'),
+    optimistic: requestFunction('optimistic')
+  }, function (err, res) {
+    if (err) return console.log('Error getting times: ' + err);
+
+    res.time = time;
+    db.get().collection('datapoints').insert(res, function (err) {
+      if (err) {
+        console.log('Mongo insert error:', err);
+      }
+    })
+  });
+}
--- index.js
+++ index.js
@@ -1,60 +1,7 @@
-var request = require('request'),
-  querystring = require('querystring'),
-  async = require('async'),
-  _ = require('underscore');
-
-var db = require('./db');
+var db = require('./db'),
+  fetch = require('./fetch');
 
 var interval = 2 * 60 * 1000; // 2 minutes
-
-var url_base = 'https://maps.googleapis.com/maps/api/directions/json?'
-var api_key = process.env.API_KEY;
-
-var origin = process.env.ORIGIN || '50 Parliament Street, Cape Town';
-var destination = process.env.DESTINATION || '60 Klipper Road, Rondebosch, Cape Town';
-
-function requestFunction(traffic_model) {
-  return function (cb) {
-    var query = {
-      origin: origin,
-      destination: destination,
-      key: api_key,
-      mode: 'driving',
-      departure_time: 'now',
-      traffic_model: traffic_model
-    };
-
-    var url = url_base + querystring.stringify(query);
-
-    request(url, function (err, resp, body) {
-      if (err) return cb(err);
-      if (resp.statusCode != 200) return cb('Request failed, status code ' + resp.statusCode);
-
-      var data = JSON.parse(body);
-      if (data.status != 'OK') return cb('Request unsuccessful, status ' + data.status);
-
-      cb(null, data.routes[0].legs[0].duration_in_traffic.value);
-    });
-  }
-}
-
-function requestAndWrite () {
-  var time = new Date();
-  async.parallel({
-    best_guess: requestFunction('best_guess'),
-    pessimistic: requestFunction('pessimistic'),
-    optimistic: requestFunction('optimistic')
-  }, function (err, res) {
-    if (err) return console.log('Error getting times: ' + err);
-
-    res.time = time;
-    db.get().collection('datapoints').insert(res, function (err) {
-      if (err) {
-        console.log('Mongo insert error:', err);
-      }
-    })
-  });
-}
 
 db.connect(function (err, database) {
   if (err) {
@@ -62,8 +9,8 @@ db.connect(function (err, database) {
     return;
   }
 
-  requestAndWrite();
+  fetch.fetchAndSave();
   setInterval(function() {
-    requestAndWrite();
+    fetch.fetchAndSave();
   }, interval);
 });
