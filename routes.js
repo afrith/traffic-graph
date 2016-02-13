9aa5703e453807883d6c7c8e85c7d5ad99e04eb8
Adrian Frith <adrian@adrianfrith.com>
2016-02-13 17:22:10 +0200

    route to get data

--- routes.js
+++ routes.js
@@ -1,8 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('./db');
+
+router.use(function (err, req, res, next) {
+  if (err == 404) {
+    res.status(404).send('Not Found');
+  } else if (err == 403) {
+    res.status(403).send('Forbidden');
+  } else if (err == 400) {
+    res.status(400).send('Bad Request');
+  } else {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/', function (req, res, next) {
   res.send('Hello');
+});
+
+router.get('/lastdata/:seconds', function (req, res, next) {
+  var seconds = parseInt(req.params.seconds);
+
+  if (isNaN(seconds) || seconds <= 0) {
+    next(400);
+    return;
+  }
+
+  var now = (new Date()).getTime();
+  var then = new Date(now - req.params.seconds * 1000);
+
+  res.type('csv');
+
+  db.get().collection('datapoints')
+    .find({time: {$gt: then}}).sort({time: 1})
+    .each(function(err, item) {
+      if (err) return next(err);
+      if (item != null) {
+        var data = [item.time.toISOString(), item.best_guess, item.pessimistic, item.optimistic];
+        res.write(data.join(',') + '\n');
+      } else {
+        res.end();
+      }
+    });
 });
 
 module.exports = router;
